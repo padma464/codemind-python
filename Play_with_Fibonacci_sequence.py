74ab80acf2ffc3ce212839e2e64e5e7c4eba7671
padma464 <114167689+padma464@users.noreply.github.com>
2022-10-04 21:40:42 +0530

    def fib_seq(n):     a,b=0,1     if n==1:         print(a)     else:         print(a,b,end=" ")     for i in range(3,n+1):         c=a+b         print(c,end=" ")         a=b         b=c
    n=int(input())
    fib_seq(n)

---
+++ Play_with_Fibonacci_sequence.py
@@ -0,0 +1,14 @@
+def fib_seq(n):
+    a,b=0,1
+    if n==1:
+        print(a)
+    else:
+        print(a,b,end=" ")
+    for i in range(3,n+1):
+        c=a+b
+        print(c,end=" ")
+        a=b
+        b=c
+        
+n=int(input())
+fib_seq(n)
